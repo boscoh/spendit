b77c0d2220021952cd54d00955ca568f980c4725
Bosco Ho <apposite@gmail.com>
2024-05-09 15:29:31 +1000

    refactor vue-plotly to composition

--- client-vue3/src/components/VuePlotly.vue
+++ client-vue3/src/components/VuePlotly.vue
@@ -2,11 +2,13 @@
   <div ref="container" :id="plotlyId"></div>
 </template>
 
-<script>
+<script setup>
 import { v4 as uuidv4 } from 'uuid'
 import Plotly from 'plotly.js-dist'
+import { defineEmits, defineProps, onBeforeUnmount, ref, watchEffect } from 'vue'
 
-const events = [
+const props = defineProps(['data', 'layout', 'config'])
+const emit = defineEmits([
   'plotly_click',
   'plotly_hover',
   'plotly_unhover',
@@ -20,61 +22,61 @@ const events = [
   'plotly_redraw',
   'plotly_animated',
   'plotly_afterplot'
-]
+])
+let plotlyId = ref(`plotly-${uuidv4()}`)
+let container = ref(null)
+let plot = null
+let resizeObserver = null
 
-let timeOutFunctionId
-
-export default {
-  name: 'VuePlotly',
-
-  data() {
-    return {
-      plotlyId: `plotly-${uuidv4()}`,
-      plot: null
-    }
-  },
-
-  props: ['data', 'layout', 'config'],
-
-  watch: {
-    data() {
-      this.reactPlot()
-    },
-    layout() {
-      this.reactPlot()
-    },
-    config() {
-      this.reactPlot()
-    }
-  },
-
-  emits: events,
-
-  mounted() {
-    this.plot = Plotly.newPlot(this.plotlyId, this.data, this.layout, this.config)
-
-    events.forEach((name) => {
-      this.$refs.container.on(name, (...args) => this.$emit(name, ...args))
-    })
-
-    this.resizeObserver = new ResizeObserver(() => {
-      clearTimeout(timeOutFunctionId) // debounce the reset
-      timeOutFunctionId = setTimeout(() => {
-        Plotly.Plots.resize(this.$refs.container)
-      }, 100)
-    })
-
-    this.resizeObserver.observe(this.$refs.container)
-  },
-
-  beforeUnmount() {
-    this.resizeObserver.disconnect()
-  },
-
-  methods: {
-    reactPlot() {
-      Plotly.react(this.plotlyId, this.data, this.layout, this.config)
-    }
+function debounce(func, timeout = 300) {
+  let timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      func.apply(this, args)
+    }, timeout)
   }
 }
+
+function resize() {
+  Plotly.Plots.resize(container.value)
+}
+
+function react() {
+  Plotly.react(plotlyId.value, props.data, props.layout, props.config)
+}
+
+function init(div) {
+  plot = Plotly.newPlot(div.id, props.data, props.layout, props.config)
+  const events = [
+    'plotly_click',
+    'plotly_hover',
+    'plotly_unhover',
+    'plotly_selecting',
+    'plotly_selected',
+    'plotly_restyle',
+    'plotly_relayout',
+    'plotly_autosize',
+    'plotly_deselect',
+    'plotly_doubleclick',
+    'plotly_redraw',
+    'plotly_animated',
+    'plotly_afterplot'
+  ]
+  events.forEach((name) => div.on(name, (...args) => emit(name, ...args)))
+  resizeObserver = new ResizeObserver(debounce(resize, 50))
+  resizeObserver.observe(div)
+}
+
+onBeforeUnmount(() => {
+  resizeObserver.disconnect()
+})
+
+watchEffect(() => {
+  if (plot) {
+    react()
+  } else if (container.value) {
+    init(container.value)
+  }
+})
 </script>
